7ffb03e4bb4f318208f1886b8cff83bd7b0a7fa9
valdivinobiava496@gmail.com <valdivinobiava496@gmail.com>
2025-11-09 16:04:42 -0300

    Commit 17

--- Statement.java
+++ Statement.java
@@ -1,4 +1,23 @@
-public class Statement {
-    
+import java.util.Enumeration;
 
+public abstract class Statement {
+    
+    protected abstract String getHeader(Customer aCustomer);
+    protected abstract String getDetail(Rental aRental);
+    protected abstract String getFooter(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeader(aCustomer); // Usa o método polimórfico
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for this rental
+            result += getDetail(each); // Usa o método polimórfico
+        }
+
+        // add footer lines
+        result += getFooter(aCustomer); // Usa o método polimórfico
+        return result;
+    }
 }
