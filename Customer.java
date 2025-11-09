b9c322a6facef9725e3952c0b5a0a4cb2fa92af1
valdivinobiava496@gmail.com <valdivinobiava496@gmail.com>
2025-11-09 10:23:07 -0300

    Commit 2

--- Customer.java
+++ Customer.java
@@ -23,25 +23,10 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
-        Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        Rental each = (Rental) rentals.nextElement();
+        
+        double thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -60,5 +45,25 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
+   }
+
+   private double amountFor(Rental each) {
+      double result = 0;
+      switch (each.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            result += 2;
+            if (each.getDaysRented() > 2)
+               result += (each.getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            result += each.getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            result += 1.5;
+            if (each.getDaysRented() > 3)
+               result += (each.getDaysRented() - 3) * 1.5;
+             break;
+      }
+      return result;
    }
 }
